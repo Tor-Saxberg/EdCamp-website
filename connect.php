4b001042b1ababa6fe44f23f323a5abf5f0c6033
Tor-Saxberg <torsaxberg@gmail.com>
2016-10-17 09:05:09 -0700

    removed confidential information...

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
-$db = 'tsaxberg';
+$db = 't_';
 $connect = 'localhost';
-$user = 'tsaxberg';
-$pass = 'Tbrudevold1';
+$user = 't_';
+$pass = '1_';
 $db = new mysqli($connect, $user, $pass, $db) or die("unable to connect");
 
 ?>
